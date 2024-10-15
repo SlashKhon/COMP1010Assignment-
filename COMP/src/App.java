a48ede6721f42f062a07436ea0441a22861269d0
krishang77631 <161427435+krishang77631@users.noreply.github.com>
2024-10-15 16:41:23 +1100

    Delete COMP directory

--- COMP/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Character Knight = new Character("", 0, 0, 0, 0, 0);
-
-    }
-}
