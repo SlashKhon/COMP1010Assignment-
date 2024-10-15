4d049cbdece29203b55933da7eac9359b6f0bfe8
krishang.nandamuri@gmail.com <krishang.nandamuri@gmail.com>
2024-10-15 16:06:19 +1100

    .

--- COMP/src/App.java
+++ COMP/src/App.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -5,3 +6,12 @@ public class App {
 
     }
 }
+=======
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // Character Knight = new Character("", 0, 0, 0, 0, 0);
+
+    }
+}
+>>>>>>> parent of e9a1df1 (idk)
--- COMP/src/Character.java
+++ COMP/src/Character.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Character {
 <<<<<<< HEAD
+<<<<<<< HEAD
     // Base attributes
     public String name;
     public int health;
@@ -148,5 +149,78 @@ public class Character {
         this.recursiveAttack(target);
     }
 =======
+>>>>>>> parent of e9a1df1 (idk)
+=======
+    //base 
+    String name;
+    int health; 
+    int maxHealthPoints;
+    
+    //Character stats
+    int strength; 
+    int defence; 
+    int intelligence; 
+    int initiative;   //character turn order 
+    
+    //Status effect 
+    boolean wounded; 
+    boolean poisoned; 
+
+    //arraylist to hold Equipment 
+    // Armour Armour;  
+    // Weapon Weapon; 
+    
+
+
+    //Character Constructor Method 
+    public Character (String name,int health, int strength,int defence,int intelligence,int initiative){
+        this.name= name;
+        this.health = health; 
+
+        this.strength = strength; 
+        this.defence = defence; 
+        this.intelligence = intelligence; 
+        this.initiative = initiative; 
+
+        this.wounded = false; 
+        this.poisoned = false; 
+
+    }
+
+    //Methods (get and set) character stats 
+    public String getName(){
+        return name; 
+    }
+
+    public int getHealth(){
+        return health; 
+    }
+
+    public void setHealth (){
+        this.health = Math.max(0,Math.min(health,maxHealthPoints));
+    }
+
+    public int getStrength(){
+        return strength; 
+    }
+
+    public int getDefence(){
+        return defence; 
+    }
+ 
+    public int getIntelligence() {
+        return intelligence;
+    }
+    
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public boolean isPoisoned() {
+        return poisoned;
+    }
+
+    
+
 >>>>>>> parent of e9a1df1 (idk)
 }
